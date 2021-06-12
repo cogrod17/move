b9a00a431223d842d3d5230b2f65a7251ccde6f9
Cole Ogrodnick <patfan8@comcast.net>
2021-06-12 10:29:46 +0700

    more styles

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -10,7 +10,6 @@ class Profile extends Component {
   render() {
     return (
       <div className="profile-container">
-        <h1>My Stats</h1>
         <ProfileInfo />
         <Summary />
         <h1>History</h1>

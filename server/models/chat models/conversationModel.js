61a38f984b585adf842a7029c87791b3dc90e0ae
Cole Ogrodnick <patfan8@comcast.net>
2021-07-18 17:40:46 +0700

    starting on chat funcationality

---
+++ server/models/chat models/conversationModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const conversationSchema = new mongoose.Schema({
+  participants: [String],
+  created_at: Date,
+});
+
+conversationSchema.virtual("conversation", {
+  ref: "Conversation",
+  localField: "_id",
+  foreignField: "conversation_id",
+});
+
+conversationSchema.pre("save", async function (next) {
+  this.created_at = new Date();
+
+  next();
+});
---
+++ server/models/chat models/messageModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  conversation_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Conversation",
+  },
+  author: String, //username or _id
+  message: String,
+  created_at: Date,
+});
+
+messageSchema.pre("save", async function (next) {
+  this.created_at = new Date();
+
+  next();
+});
